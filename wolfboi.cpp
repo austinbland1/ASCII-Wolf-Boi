12a2e3c476904a288550ebc17516b00865c0f14c
Austin Bland <45363385+austinbland1@users.noreply.github.com>
2018-11-30 08:57:17 -0600

    Add files via upload

---
+++ wolfboi.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "                                                                            ,aa,       ,aa " << endl;
+    cout << "                                                                        d   b    ,d ,`b " << endl;
+    cout << "                                                                     ,dP a   b,ad8  8 8 " << endl;
+    cout << "                                                                     d8  8  ,88888a 8 8 " << endl;
+    cout << "                                                                    d8baa8ba888888888a8 " << endl;
+    cout << "                                                                 ,ad888888888YYYY888YYY, " << endl;
+    cout << "                                                              ,a888888888888     8P    b " << endl;
+    cout << "                                                          ,aad8888tt,8888888b (0 `8, 0 8 " << endl;
+    cout << "                      ____________________________,,aadd888ttt8888ttt 8 I   Yb,   `Ya  8 " << endl;
+    cout << "                ,aad8888b888888aab8888888888b,     ,aatPt888ttt8888tt 8,`b,    Ya,. ` aP " << endl;
+    cout << "            ,ad88tttt8888888888888888888888888ttttt888ttd88888ttt8888tt,t  ba,.  ` `d888 " << endl;
+    cout << "         ,d888tttttttttttttt888888888888888888888888ttt8888888888ttt888ttt,    a,   `88 " << endl;
+    cout << "        a888tttttttttttttttttttttttttt8888888888888ttttt88888ttt888888888tt,    `  8 " << endl;
+    cout << "       d8P ' ,tttttttttttttttttttttttttttttttttt88tttttt888tttttttt8a 8888ttt,   ,8' " << endl;
+    cout << "      d8tb    ,tt     tttttttttttttttttttttttttttttttttt88ttttttttttt, Y888tt   ,8 " << endl;
+    cout << "      88tt)               t  ttttt                 tttttYttttttttttttt,   8ttb,a8 " << endl;
+    cout << "      88tt                    ` b'                    t'ttttttttttt t t   t taP " << endl;
+    cout << "      8tP                       `b                       ,tttttt       tt, ,8 " << endl;
+    cout << "     (8tb  b,                    `b,                 a,  tttttt'          dP' " << endl;
+    cout << "     I88tb `8,                    `b                d    tttttt        ,aP " << endl;
+    cout << "     8888tb `8,                   ,P               d'     tt  t'    ,a8P " << endl;
+    cout << "    I888ttt,  b                  ,8'              ,8        tt   ,d d ' " << endl;
+    cout << "   ,888tttt'  8b               ,dP                Y8        tt ,d ,d' " << endl;
+    cout << " ,d888ttttP  d 8b            ,dP'                   b,       ttP' d " << endl;
+   cout << ",d888ttttPY ,d' dPb,        ,dP'                       b,     t8   8 " << endl;
+  cout << "d888tttt8  ,d  ,d   8      ,d                           `b      P   8 " << endl;
+ cout << "d888tt88888d  ,d   ,d     ,d                              8      I   8 " << endl;
+cout << "d888888888P  ,d   ,d     ,d                                8      I   8 " << endl;
+cout << "88888888P' ,d    (P'    d                                  8      8   8 " << endl;
+ cout << "8P ' 8   ,8'    Ib    d                                   Y      8   8 " << endl;
+    cout << "  8   d      `8    8                                   `b     8   Y " << endl;
+    cout << "  8   8       8,   8,                                   8     Y   `b " << endl;
+    cout << "  8   Y,      `b   `b                                   Y     `b   `b " << endl;
+    cout << "  Y,    ba,    `b   `b,                                 `b     8,   ` ba, " << endl;
+    cout << "    b,    8     `b    `  b                               `b     `Yaa,adP " << endl;
+    cout << "          '      `baaaaaaP                                `YaaaadP ' " << endl;
+}
